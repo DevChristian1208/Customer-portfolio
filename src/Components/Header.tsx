b8ebdb27781fcd1aa08a3adff97570bfb7202797
Christian <christian.pressig@web.de>
2025-06-03 20:46:36 +0200

    Header and Hero RPS
    Added Media Queries with Tailwind

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 const menuItems = [
   { href: "#home", label: "Home" },
@@ -28,13 +29,12 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 right-0 z-10 transition-all duration-300 ${
-        scrolled ? "bg-white shadow-md" : "bg-transparent"
+      className={`fixed top-0 left-0 right-0 z-10 transition-all duration-300 h-[70px] ${
+        scrolled ? "bg-white shadow-md" : "bg-white lg:bg-transparent"
       }`}
     >
-      <div className="container mx-auto px-4">
-        <div className="w-full h-auto flex items-center justify-between py-[30px] transition-all duration-300">
-          {/* Logo links */}
+      <div className="max-w-[1600px] w-full px-6 sm:px-10 mx-auto">
+        <div className="w-full h-[70px] flex items-center justify-between py-[30px] transition-all duration-300">
           <div
             className={`logo text-xl font-bold font-['Press_Start_2P'] ${
               scrolled ? "text-[#ea4343]" : "text-[#ea4343]"
@@ -43,10 +43,9 @@ const Header = () => {
             &lt;CS&gt;
           </div>
 
-          {/* Menü oder nur Sprache */}
           {!isSimplePage ? (
             <div className="menu flex items-center">
-              <ul className="flex m-0 list-none">
+              <ul className="flex m-0 list-none hidden lg:flex">
                 {menuItems.map((item, index) => (
                   <li key={index} className="inline-block mr-[45px] last:mr-0">
                     <a
@@ -66,13 +65,23 @@ const Header = () => {
                   scrolled ? "text-black" : "text-white"
                 }`}
               >
-                <span className="cursor-pointer hover:text-[#ea4343]">EN</span>
-                <span className="mx-2">|</span>
-                <span className="cursor-pointer hover:text-[#ea4343]">DE</span>
+                <span className="cursor-pointer hover:text-[#ea4343] hidden lg:flex">
+                  EN
+                </span>
+                <span className="mx-2 hidden lg:flex">|</span>
+                <span className="cursor-pointer hover:text-[#ea4343] hidden lg:flex">
+                  DE
+                </span>
+                <Image
+                  className="flex lg:hidden cursor-pointer"
+                  src="/burgermenu.png"
+                  alt="Burger Menu"
+                  width={30}
+                  height={30}
+                />
               </div>
             </div>
           ) : (
-            // Nur Sprache rechts
             <div
               className={`flex items-center font-semibold ${
                 scrolled ? "text-black" : "text-white"
